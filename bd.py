9efebe4c3c230239dba8a3305674a48c210f2c9e
Gener Emmanuel Poot Can <60999054+GenerEmmanuelPootCan@users.noreply.github.com>
2021-09-24 18:39:06 -0500

    Update bd.py

--- bd.py
+++ bd.py
@@ -1,4 +1,8 @@
-import pyodbc 
+import pyodbc
+import pymysql
+
+#-----------------------------------------------------------------------------------------------------------------------
+#CONEXION CON SQL SERVER
 
 direccion_servidor = 'localhost'
 nombre_bd = 'cinebd'
@@ -8,7 +12,19 @@ try:
     conexion = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + 
                               direccion_servidor+';DATABASE='+nombre_bd+';UID='+nombre_usuario+';PWD=' + password)
     print("\n"*2)
-    print("conexión exitosa")
+    print("conexión exitosa con: SQL Server")
     
 except Exception as e:
     print("Ocurrió un error al conectar a SQL Server: ", e) 
+
+#----------------------------------------------------------------------------------------------------------------------
+#CONEXION CON MYSQL
+
+try:
+	conexion = pymysql.connect(host='localhost',
+                             user='root',
+                             password='',
+                             db='peliculas')
+	print("Conexión exitosa con: MySQL")
+except (pymysql.err.OperationalError, pymysql.err.InternalError) as e:
+	print("Ocurrió un error al conectar: ", e)
